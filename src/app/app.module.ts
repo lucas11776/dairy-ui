c6e4df6e99039316c8703d7facb0afe12c93eb37
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-08-09 23:13:25 +0200

    Added dairy component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DairyComponent } from './dairy/dairy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DairyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
